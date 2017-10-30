f367156833d7c332bbdfe150779f59beafff1785
1blockologist <1blockologist>
2017-10-30 11:36:35 -0700

    favicons

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="shortcut icon" href="favicon.ico">
-
         <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
 
         <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,700,700i" rel="stylesheet">
         <link rel="stylesheet" href="css/general.css">
