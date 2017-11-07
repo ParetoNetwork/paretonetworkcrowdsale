6378826353906b9e53462f2cafa614bc0ec6f407
1blockologist <1blockologist>
2017-11-07 08:33:08 -0800

    pareto google analytics

--- crowdsale.php
+++ crowdsale.php
@@ -26,6 +26,24 @@
 
         <link rel="stylesheet" href="css/general.css">
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108222959-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-108222959-1');
+        </script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j = d.createElement (s), df = l = 'dataLayer' '& l =' + l ''; j.async = true; j.src =?
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQ825N7');</script>
+        <!-- End Google Tag Manager -->
+
         <script>
             if ( localStorage.getItem("acception") != 'true' ) {
                 location.href = 'index.php';
@@ -37,6 +55,10 @@
 
     <body>
 
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ825N7"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
 
         <!--=================================================================================================-->
 
