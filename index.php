c6454485031585782b48c707759bab5c21cd33a1
Eric Lamison-White <eric@blockology.org>
2018-01-14 22:58:18 -0800

    headers to un cache

--- index.php
+++ index.php
@@ -43,6 +43,11 @@
 
     <body>
 
+     <?php header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache'); ?>
+
         <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ825N7"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
